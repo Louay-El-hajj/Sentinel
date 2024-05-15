184d52aa44c0031878bf7eb6df6e064367f7c5ab
Louayhj <139022326+Louayhj@users.noreply.github.com>
2024-05-15 13:25:48 +0300

    members screen

---
+++ flutter_app/lib/screens/AboutUs/teammembers.dart
@@ -0,0 +1,20 @@
+class TeamMembers {
+  String imgpath;
+  String name;
+  String qual;
+  String team;
+  TeamMembers(
+      {required this.imgpath,
+      required this.name,
+      required this.team,
+      required this.qual});
+}
+
+List<TeamMembers> Members = [
+  TeamMembers(
+      imgpath: "#",
+      name: "Louay El Hajj",
+      team: "FSW27",
+      qual: "---"),
+  
+];
