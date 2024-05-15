b2be54e95039876f372e00645c04714d41e527e5
Louayhj <139022326+Louayhj@users.noreply.github.com>
2024-05-15 13:24:18 +0300

    ui

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -1,18 +1,50 @@
+import 'package:animated_splash_screen/animated_splash_screen.dart';
+import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
-import 'pages/home_page.dart';
+import 'package:smarthelmet/screens/SignIn/SignIn.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:smarthelmet/pageview.dart';
+import 'package:smarthelmet/screens/Splash/splash.dart';
+import 'package:smarthelmet/shared/network/local/cache_helper.dart';
+import 'firebase_options.dart';
+import 'package:page_transition/page_transition.dart';
 
-void main() {
-  runApp(const MyApp());
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await CachHelper.init();
+  // await Firebase.initializeApp();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  FirebaseDatabase.instance;
+  var userID = await CachHelper.getData(key: "uid");
+  Widget startWidget = const SignInScreen();
+  if (userID != null) {
+    startWidget = const PageViewScreen();
+  }
+  runApp(MyApp(startWidget: startWidget));
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
+  final Widget startWidget;
+  const MyApp({
+    super.key,
+    required this.startWidget,
+  });
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: HomePage(),
-    );
+        title: '',
+        theme: ThemeData(
+          primarySwatch: Colors.amber,
+        ),
+        debugShowCheckedModeBanner: false,
+        home: AnimatedSplashScreen(
+            duration: 3000,
+            splashIconSize: 400,
+            splash: const SplashScreen(),
+            nextScreen: startWidget,
+            pageTransitionType: PageTransitionType.fade,
+            backgroundColor: Colors.white));
   }
 }
