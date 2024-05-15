f3a4f715726ea4fab6896b24e1c96e23ddd7cf12
Louayhj <139022326+Louayhj@users.noreply.github.com>
2024-05-15 13:35:32 +0300

    emergency contacts

---
+++ flutter_app/lib/shared/screens/Emergency Contacts/emergency.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'home_tabs.dart';
+class EmergencyScreen extends StatefulWidget {
+  late String index;
+  EmergencyScreen(this.index, {super.key});
+
+  @override
+  State<EmergencyScreen> createState() => _EmergencyScreenState();
+}
+
+class _EmergencyScreenState extends State<EmergencyScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Emergency Help',
+          style: TextStyle(
+            fontSize: 26.0,
+            fontWeight: FontWeight.bold,
+            letterSpacing: 2.0,
+            color: Colors.white,
+            fontFamily: 'Ubuntu',
+          ),
+        ),
+        backgroundColor: Colors.amber,
+        elevation: 0.0,
+        leading: const Icon(
+           Icons.call
+        ),
+      ),
+      body:  HomeScreenTabs(widget.index),
+          
+    );
+  }
+}
